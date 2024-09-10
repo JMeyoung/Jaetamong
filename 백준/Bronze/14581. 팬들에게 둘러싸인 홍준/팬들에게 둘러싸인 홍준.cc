bfb1dfe83916082b2f93d1bc2f6980ac7ecf0033
Jaetamong <163428169+JMeyoung@users.noreply.github.com>
2024-09-10 20:28:58 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char name[51];
+    scanf("%s", name);
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n", name);
+    printf(":fan::fan::fan:\n");
+    return 0;
+}
