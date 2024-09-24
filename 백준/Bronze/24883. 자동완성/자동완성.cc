9190ea6c9a964a6540a7be83aee734a8f1af23e8
Jaetamong <163428169+JMeyoung@users.noreply.github.com>
2024-09-24 23:17:44 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()  {
+    char input;
+    scanf("%c", &input);
+
+    if (input == 'N' || input == 'n') {
+        printf("Naver D2");
+    }
+    else {
+        printf("Naver Whale");
+    }
+    return 0;
+}
