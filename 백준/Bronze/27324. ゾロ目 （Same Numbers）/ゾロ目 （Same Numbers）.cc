1b7f9688a59a4da11bfb30cda667b33f2811ea16
Jaetamong <163428169+JMeyoung@users.noreply.github.com>
2024-10-05 00:43:16 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    if (N / 10 == N % 10) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    return 0;
+}
