8899e6dcae22d1dc01f3c30d130ff535f18f8239
Jaetamong <163428169+JMeyoung@users.noreply.github.com>
2024-10-17 00:15:35 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++){
+        scanf("%d", &k);
+        printf("%d %d\n", k, k);}
+    return 0;
+}
