9e37c3b2523299682e735db5e56aa90dcab59a83
Jaetamong <163428169+JMeyoung@users.noreply.github.com>
2024-10-22 00:42:05 +0900

    [Bronze V] Title: Rectangles, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+    
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
