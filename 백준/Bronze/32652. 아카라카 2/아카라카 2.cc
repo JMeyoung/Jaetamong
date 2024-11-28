364e5d08c3ef0f02239a0c24f9686b58e491f1b2
Jaeyoung Jang <163428169+JMeyoung@users.noreply.github.com>
2024-11-28 17:20:52 +0900

    [Bronze III] Title: 아카라카 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int K;
+    scanf("%d", &K);
+
+    printf("AKARAKA");
+    for (int i = 1; i < K; i++) {
+        printf("RAKA");
+    }
+    printf("\n");
+
+    return 0;
+}
