ac393d76661527a3355a30c322ce1626cf520458
Jaeyoung Jang <163428169+JMeyoung@users.noreply.github.com>
2024-12-07 00:48:49 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int s1, s2;
+    scanf("%d %d", &s1, &s2);
+
+    if (s1 * 2 >= s2) {
+        printf("E\n");
+    } else {
+        printf("H\n");
+    }
+
+    return 0;
+}
