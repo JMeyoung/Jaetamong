eeef671728cd62a26f4dd96a381d5b862be0a4b5
Jaeyoung Jang <163428169+JMeyoung@users.noreply.github.com>
2025-03-25 18:51:32 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -1,25 +1,16 @@
 #include <stdio.h>
-int main(void){
+
+int main() {
     int x, y, w, h;
+    
     scanf("%d %d %d %d", &x, &y, &w, &h);
 
-    int Leftside = x;
-    int Rightside = w - x;
-    int Downside = y;
-    int Upside = h - y;
+    int min_dist = x;
+    if (w - x < min_dist) min_dist = w - x;
+    if (y < min_dist) min_dist = y;
+    if (h - y < min_dist) min_dist = h - y;
 
-    int minimum_value = Leftside;
-    if (Rightside < minimum_value){
-        minimum_value = Rightside;
-    }
-    if (Downside < minimum_value){
-        minimum_value = Downside;
-    }
-    if (Upside < minimum_value){
-        minimum_value = Upside;
-    }
-
-    printf("%d\n", minimum_value);
+    printf("%d\n", min_dist);
 
     return 0;
 }
