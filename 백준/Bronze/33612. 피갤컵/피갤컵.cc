a84bfe41cd9cf58394a08f8da693c62db06572bc
Jaeyoung Jang <163428169+JMeyoung@users.noreply.github.com>
2025-04-14 01:17:11 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int year = 2024;
+    int month = 8 + (N - 1) * 7;
+
+    year += (month - 1) / 12;
+    month = (month - 1) % 12 + 1;
+
+    cout << year << " " << month << endl;
+
+    return 0;
+}
