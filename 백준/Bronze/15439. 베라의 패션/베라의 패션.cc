ed7e993d482ec1728df0fc8a45ded9aaa7d9edd2
Jaeyoung Jang <163428169+JMeyoung@users.noreply.github.com>
2025-04-23 23:58:34 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+  int N;
+  cin >> N;
+
+  int combination = 0;
+
+  combination = N * (N - 1);
+
+  cout << combination ;
+  
+  return 0;
+}
