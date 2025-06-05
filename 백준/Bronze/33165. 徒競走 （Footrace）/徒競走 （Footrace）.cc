a3ee9d1ba121efd76ffacd4a7d7e8083905125df
Jaeyoung Jang <163428169+JMeyoung@users.noreply.github.com>
2025-06-06 01:05:14 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    int V;
+    int distance;
+
+    scanf("%d %d", &T, &V);
+
+    distance = T * V;
+
+    printf("%d\n", distance);
+
+    return 0;
+}
