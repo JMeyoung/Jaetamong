dc68de5844151caac67c8a43ca54d20b48ac0dd3
Jaeyoung Jang <163428169+JMeyoung@users.noreply.github.com>
2025-10-18 00:30:26 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int m;
+    int n;
+    int t;
+
+    scanf("%d", &m);
+    scanf("%d", &n);
+    scanf("%d", &t);
+
+    int total_distance = m * 2;
+
+    printf("%d\n", total_distance);
+
+    return 0;
+}
+
