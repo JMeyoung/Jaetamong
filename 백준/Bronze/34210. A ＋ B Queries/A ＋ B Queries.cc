79144590ad2d16ad3e8e5b1341860c0b0c19b021
Jaeyoung Jang <163428169+JMeyoung@users.noreply.github.com>
2026-01-20 09:52:53 +0900

    [Bronze IV] Title: A + B Queries, Time: 32 ms, Memory: 8288 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34210. A ＋ B Queries/A ＋ B Queries.cc
@@ -0,0 +1,14 @@
+#include "aplusb.h"
+#include <vector>
+
+static std::vector<int> A_global;
+static std::vector<int> B_global;
+
+void initialize(std::vector<int> A, std::vector<int> B) {
+    A_global = A;
+    B_global = B;
+}
+
+int answer_question(int i, int j) {
+    return A_global[i] + B_global[j];
+}
